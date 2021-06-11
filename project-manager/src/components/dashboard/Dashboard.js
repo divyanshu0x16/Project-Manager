d5f27da271a779487dd74be0e0515e7a4b53a0e8
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-06-12 01:03:14 +0530

    Adding Dummy Data

--- project-manager/src/components/dashboard/Dashboard.js
+++ project-manager/src/components/dashboard/Dashboard.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
+import { connect } from 'react-redux';
 
-const Dashboard = () => {
+const Dashboard = ({projects}) => {
   return (
     <div>
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-              <ProjectList/>
+            <ProjectList projects={projects}/>
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -19,4 +20,12 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+//This function is calleb whenever the store state changes
+const mapStateToProps = (state) => {
+  return {
+    //State has a project object (rootReducer), which has projects object (projectsReducer)
+    projects: state.project.projects,
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
