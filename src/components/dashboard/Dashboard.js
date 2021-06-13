47620b3a5a48c5f065f0d8dee873f7ebc5c87f38
divyanshu0x16 <divyanshu22.dm@gmail.com>
2021-06-13 14:52:04 +0530

    Showing Notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,22 +6,22 @@ import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { Redirect } from 'react-router-dom';
 
-const Dashboard = ({ projects, auth }) => {
-  if (!auth.uid) return <Redirect to='/signin' />
-    return (
-      <div>
-        <div className="dashboard container">
-          <div className="row">
-            <div className="col s12 m6">
-              <ProjectList projects={projects} />
-            </div>
-            <div className="col s12 m5 offset-m1">
-              <Notifications />
-            </div>
+const Dashboard = ({ projects, auth, notifications }) => {
+  if (!auth.uid) return <Redirect to="/signin" />;
+  return (
+    <div>
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList projects={projects} />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notifications notifications={notifications}/>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 //This function is calleb whenever the store state changes
@@ -30,10 +30,21 @@ const mapStateToProps = (state) => {
     //State has a project object (rootReducer), which has projects object (projectsReducer)
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications,
   };
 };
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect(() => ['projects'])
+  firestoreConnect(() => [
+    {
+      collection: 'projects',
+      orderBy: ['createdAt', 'desc'],
+    },
+    {
+      collection: 'notifications',
+      limit: 3,
+      orderBy: ['time', 'desc'],
+    },
+  ])
 )(Dashboard);
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 const Notifications = (props) => {
-    return(
-        <div className="section">
-            <div className="card">
-                <div className="card-content">
-                    <span className='card-title'>Notifications</span>
-                    <ul className='notifications'>
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
-                    </ul>
-                </div>
-            </div>
+  const { notifications } = props;
+  return (
+    <div className="section">
+      <div className="card">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            { notifications && notifications.map(item => {
+                return (
+                    <li key={item.id}>
+                        <span className="pink-text">{item.user} </span>
+                        <span> {item.content}</span>
+                        <div className="grey-text note-date">
+                            {moment(item.time.toDate()).fromNow()}
+                        </div>
+                    </li>
+                )
+            })}
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Notifications
+export default Notifications;
